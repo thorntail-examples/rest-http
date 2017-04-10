15b084a1016dabf1967b43e3919cf71319e753e6
Ken Finnigan <ken@kenfinnigan.me>
2017-04-10 17:09:24 -0400

    Update to latest booster-parent that brings in openshift-test-utils and adjust test dependencies

--- src/test/java/io/openshift/booster/OpenshiftIT.java
+++ src/test/java/io/openshift/booster/OpenshiftIT.java
@@ -1,11 +1,18 @@
 package io.openshift.booster;
 
+import java.util.concurrent.TimeUnit;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import io.openshift.booster.test.OpenShiftTestAssistant;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.expect;
-import static io.restassured.RestAssured.given;
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.containsString;
 
 /**
@@ -13,7 +20,7 @@ import static org.hamcrest.Matchers.containsString;
  */
 public class OpenshiftIT {
 
-    private static final OpenshiftTestAssistant openshift = new OpenshiftTestAssistant();
+    private static final OpenShiftTestAssistant openshift = new OpenShiftTestAssistant();
 
     @BeforeClass
     public static void setup() throws Exception {
@@ -22,6 +29,17 @@ public class OpenshiftIT {
 
         // wait until the pods & routes become available
         openshift.awaitApplicationReadinessOrFail();
+
+        await().atMost(5, TimeUnit.MINUTES).until(() -> {
+            try {
+                Response response = get();
+                return response.getStatusCode() == 200;
+            } catch (Exception e) {
+                return false;
+            }
+        });
+
+        RestAssured.baseURI = RestAssured.baseURI + "/api/greeting";
     }
 
     @AfterClass
@@ -31,23 +49,21 @@ public class OpenshiftIT {
 
     @Test
     public void testServiceInvocation() {
-
-        expect().
-                statusCode(200).
-                body(containsString("Hello, World!")).
-                when().
-                get(openshift.getBaseUrl() + "/api/greeting");
+        when()
+                .get()
+        .then()
+                .statusCode(200)
+                .body(containsString("Hello, World!"));
     }
 
     @Test
     public void testServiceInvocationWithParam() {
-
-        given().
-                param("name", "Peter").
-                expect().
-                statusCode(200).
-                body(containsString("Hello, Peter!")).
-                when().
-                get(openshift.getBaseUrl() + "/api/greeting");
+        given()
+                .queryParam("name", "Peter")
+        .when()
+                .get()
+        .then()
+                .statusCode(200)
+                .body(containsString("Hello, Peter!"));
     }
 }
