20d478955d97480a9ead00835153468287613ab1
Ladislav Thon <lthon@redhat.com>
2017-09-12 12:50:24 +0200

    move from OpenShiftTestAssistant to Arquillian Cube

--- src/test/java/io/openshift/booster/OpenshiftIT.java
+++ src/test/java/io/openshift/booster/OpenshiftIT.java
@@ -18,14 +18,15 @@
 
 package io.openshift.booster;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.Response;
-import io.openshift.booster.test.OpenShiftTestAssistant;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.arquillian.cube.openshift.impl.enricher.RouteURL;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static com.jayway.restassured.RestAssured.get;
@@ -36,33 +37,23 @@ import static org.hamcrest.Matchers.containsString;
 /**
  * @author Heiko Braun
  */
+@RunWith(Arquillian.class)
 public class OpenshiftIT {
 
-    private static final OpenShiftTestAssistant openshift = new OpenShiftTestAssistant();
+    @RouteURL("wfswarm-rest-http")
+    private URL url;
 
-    @BeforeClass
-    public static void setup() throws Exception {
-        // the application itself
-        openshift.deployApplication();
-
-        // wait until the pods & routes become available
-        openshift.awaitApplicationReadinessOrFail();
-
+    @Before
+    public void setup() {
         await().atMost(5, TimeUnit.MINUTES).until(() -> {
             try {
-                Response response = get();
-                return response.getStatusCode() == 200;
+                return get(url).getStatusCode() == 200;
             } catch (Exception e) {
                 return false;
             }
         });
 
-        RestAssured.baseURI = RestAssured.baseURI + "/api/greeting";
-    }
-
-    @AfterClass
-    public static void teardown() throws Exception {
-        openshift.cleanup();
+        RestAssured.baseURI = url + "api/greeting";
     }
 
     @Test
